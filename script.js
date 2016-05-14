f5fa78044a515e3f52890d9e59de036249177528
Brian Chan <brian.chan@medecision.com>
2016-05-13 20:49:23 -0400

    add projects tab

--- script.js
+++ script.js
@@ -18,23 +18,22 @@
                 controller  : 'aboutController'
             })
 
-            // route for the contact page
-            .when('/contact', {
-                templateUrl : 'pages/contact.html',
-                controller  : 'contactController'
+            // route for the projects page
+            .when('/projects', {
+                templateUrl : 'pages/projects.html',
+                controller  : 'projectsController'
             });
     });
 
     // create the controller and inject Angular's $scope
     myApp.controller('mainController', function($scope) {
-        // create a message to display in our view
-        //$scope.message = 'Everyone come and see how good I look!';
+      
     });
 
     myApp.controller('aboutController', function($scope) {
-        //$scope.message = 'Look! I am an about page.';
+      
     });
 
-    myApp.controller('contactController', function($scope) {
-        //$scope.message = 'Contact us! JK. This is just a demo.';
+    myApp.controller('projectsController', function($scope) {
+        
     });
